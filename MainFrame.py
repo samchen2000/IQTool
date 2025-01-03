71a86acdc049ba380e84a4eada865835ed7f4db0
samchen2000 <sam.pitcher@msa.hinet.net>
2025-01-03 18:42:10 +0800

    add math window
    1. add math window
    2. modify import

--- MainFrame.py
+++ MainFrame.py
@@ -238,7 +238,7 @@ class Main_GUI():
         self.mymenu.add_command(label="二值化", command=self.command_threshold)
         self.mygui.config(menu=self.mymenu)
         # RGB 菜單
-        self.mymenu.add_command(label="RGB", command=self.detect_cam)
+        self.mymenu.add_command(label="RGB", command=self.face_detect_cam)
         self.mygui.config(menu=self.mymenu)
         # 幫助菜單
         self.mymenu.add_command(label="幫助", command=self.command_help)
@@ -386,6 +386,53 @@ class Main_GUI():
 
         cap.release()
         cv2.destroyAllWindows()
+    def face_detect_cam(self):  #測試人臉跟眼睛的功能
+        source = "rtsp://192.168.1.10/video0"
+        face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+        eyes_cascade = cv2.CascadeClassifier("haarcascade_eye.xml")
+        vid_cam = cv2.VideoCapture(index=1)
+
+        #先设置参数，然后读取参数
+        vid_cam.set(3,1280)
+        vid_cam.set(propId=4,value=720)
+        arr = np.array([1, 2, 3, 4, 5])
+        print(arr)    # [1 2 3 4 5]
+        print("width={}".format(vid_cam.get(3)))
+        print("height={}".format(vid_cam.get(4)))
+        print("FPS={}".format(vid_cam.get(5)))
+        print("Brightness={}".format(vid_cam.get(10)))
+        print("Contrast={}".format(vid_cam.get(11)))
+        print("Saturation={}".format(vid_cam.get(12)))
+        print("HUE={}".format(vid_cam.get(13)))
+        print("GAIN={}".format(vid_cam.get(14)))
+        print("exposure={}".format(vid_cam.get(15)))
+        print("White Balance={}".format(vid_cam.get(17)))
+        if not vid_cam.isOpened():
+            print('無法打開攝影機')
+            exit()
+
+        if not vid_cam.isOpened():
+            print("Cannot open camera")
+            exit()
+        while True:
+            ret, frame = vid_cam.read()
+            if not ret:
+                print("Cannot receive frame")
+                break
+            frame = cv2.resize(frame,(640,480))              # 縮小尺寸，避免尺寸過大導致效能不好
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)   # 將鏡頭影像轉換成灰階
+            faces = face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=3, minSize=(40,40), )      # 偵測人臉
+            for (x, y, w, h) in faces:
+                cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)   # 標記人臉
+                #print('X='+str(x), 'Y='+str(y))
+            eyes = eyes_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=3, minSize=(40, 40), )    #偵測眼睛
+            for (ex, ey, ew, eh) in eyes:
+                cv2.rectangle(frame,(ex,ey),(ex+ew,ey+eh),(255,0,0),2)    #標記眼睛
+            cv2.imshow('UVC Cam face detect', frame)
+            if cv2.waitKey(1) == ord('q'):
+                break
+        vid_cam.release()
+        cv2.destroyAllWindows()            
     def cam_data(self):
         pass
     
--- math_test.py
+++ math_test.py
@@ -1,4 +1,7 @@
 import math
+import tkinter as tk
+from tkinter import *
+import cv2
 
 angle = 90 #設定角度
 angle_1 = 60
@@ -9,5 +12,76 @@ print(f'radians={radians}')
 print(f'sin value={sin_value}')
 print(f'angle_value={angle_value}')
 
-def FOV(math):
-    pass
+class MAIN_GUI():
+    def __init__(mathgui, w=920, h=640):
+        left = 30
+        top = 30
+        mathgui.width = w
+        mathgui.height = h
+        mathgui.mygui = Tk(className = "數學計算介面")
+        mathgui.mygui.geometry(f'{mathgui.width}x{mathgui.height}+{left}+{top}')
+        mathgui.mygui.resizable(False, False)
+        mathgui.mygui.grid()
+        mathgui.createWiget()
+        
+    def createWiget(mathGUI):
+        # ---------------菜單欄--------------------
+        mathGUI.mymenu = Menu(mathGUI.mygui)
+        # 建立下拉選單
+
+        mathGUI.mymenu.add_command(label="AAA", menu=A_filemenu)
+        A_filemenu = Menu(mathGUI.mymenu, )
+        A_filemenu.add_command(label='A____A')
+        A_filemenu.add_command(label="A____B")
+        A_filemenu.add_command(label="A____C")
+        
+        B_filemenu = Menu(mathGUI.mymenu, )
+        mathGUI.mymenu.add_command(label="BBBB", menu=B_filemenu)
+        B_filemenu.add_command(label="B______A")
+        B_filemenu.add_command(label="B______B")
+        B_filemenu.add_command(label="B______C")
+        
+        D_filemenu = Menu(mathGUI.mymenu, )
+        mathGUI.mymenu.add_command(label="DDD", menu=D_filemenu)
+        D_filemenu.add_command(label="D______A")
+        D_filemenu.add_command(label="D______B")
+        D_filemenu.add_command(label="D______C")
+        
+        mathGUI.mymenu.add_command(label="EEE", menu=E_filemenu)
+        E_filemenu = Menu(mathGUI.mymenu, )
+        E_filemenu.add_command(label="E______A")
+        E_filemenu.add_command(label="E______B")
+        E_filemenu.add_command(label="E______C")
+        
+        mathGUI.mymenu.add_command(label="FFF", menu=F_filemenu)
+        F_filemenu = Menu(mathGUI.mymenu, )
+        F_filemenu.add_command(label="F______A")
+        F_filemenu.add_command(label="F______B")
+        F_filemenu.add_command(label="F______C")
+        
+        mathGUI.mymenu.add_command(label="GGG")
+        #顯示菜單對象
+        mathGUI.mygui.config(menu=mathGUI.mymenu)
+        mathGUI.lable = Label(mathGUI.mygui, width=mathGUI.width, height=mathGUI.height)
+        mathGUI.lable.place(relx=0.01, rely=0.01)
+        
+    def FOV(mathGUI):
+        pass
+    def color(mathGUI):
+        pass
+    def exposure(mathGUI):
+        pass
+    def gamma(mathGUI):
+        pass
+class IQ_MATH():
+    def math_fov(mathIQ):
+        pass
+    def math_color(mathIQ):
+        pass
+    def mathexposur(mathIQ):
+        pass
+    def gamme(mathGUI):
+        pass    
+if __name__ == "__main__":
+    root = MAIN_GUI()
+    mainloop()
